9cd95acc505cef325099d76c09ac7c76002dc4b3
juanasalamanco <juanasalamanco@gmail.com>
2022-04-12 08:08:30 -0300

    update

--- Assets/validarcontrasena.cs
+++ Assets/validarcontrasena.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class validarcontrasena : MonoBehaviour
 {
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -15,8 +20,17 @@ public class validarcontrasena : MonoBehaviour
     {
         
     }
-    public void validarcontraseña()
+    public void validarcontrasenia()
     {
+        contraseniaUsuario = ingresoUsuario.text;
 
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
